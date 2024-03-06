42b437e0a33bab66d146e7a3dc9626f15db715cb
Luka <lukadbarth@hotmail.com>
2024-03-06 13:38:47 -0300

    Exercising class creation!! =D

---
+++ Exercício/classe-musica.py
@@ -0,0 +1,19 @@
+class Musica:
+  nome = ''
+  artista = ''
+  duracao = float
+
+musica1 = Musica()
+Musica.nome = 'Balada Louca'
+Musica.artista = 'Munhoz & Mariano'
+Musica.duracao = 3
+
+musica2 = Musica()
+Musica.nome = 'Bêbado Famoso'
+Musica.artista = 'Murilo Huff'
+Musica.duracao = 3.18
+
+musica3 = Musica()
+Musica.nome = 'Mensagem Pra Ela'
+Musica.artista = 'César Menotti & Fabiano'
+Musica.duracao = 2.43
